4c9cbc94cfcd829463f826ce6c3ee61888825bcc
sigmadev92 <devanshofficial2000@gmail.com>
2025-11-02 10:03:35 +0530

    setup swagger and made doc for root endpoint

---
+++ src/config/urls.js
@@ -0,0 +1,5 @@
+import { PORT } from "./env.js";
+
+const serverURL = `http://localhost:${PORT}`;
+
+export { serverURL };
---
+++ src/docs/swagger/swaggerOptions.js
@@ -0,0 +1,20 @@
+import { serverURL } from "../../config/urls.js";
+
+const swaggerOptions = {
+  definition: {
+    openapi: "3.0.0", // OpenAPI version
+    info: {
+      title: "Blogging App API",
+      version: "1.0.0",
+      description: "API documentation for the backend of Blogging (MERN)",
+    },
+    servers: [
+      {
+        url: serverURL, // Backend server URL
+      },
+    ],
+  },
+  apis: ["./src/docs/swagger/route_docs/*.js"],
+  // Path to your route files
+};
+export default swaggerOptions;
